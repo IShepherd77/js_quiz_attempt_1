888cd712c35b036ef7b7f2633368abbb97a98316
IShepherd77 <ianshepherd77@gmail.com>
2020-03-28 13:46:31 +0000

    got answers to show

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choices-text"));
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
 let acceptingAnswers = false;
